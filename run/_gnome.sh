b9f57e8cb0c0a818d96d547982a08163c3db06e3
André Albano <andre_barros@id.uff.br>
2025-08-24 23:03:18 -0300

    fix: changed mode of chmod_all within gnome

--- run/_gnome.sh
+++ run/_gnome.sh
@@ -4,7 +4,8 @@ sudo sed -i '/\[multilib\]/,/Include/ s/^#//' /etc/pacman.conf && sudo pacman -S
 sudo pacman -Syu
 sudo pacman -S --noconfirm gnome gnome-tweaks gnome-browser-connector wget curl
 
-
+chmod +x ./chmod_all.sh
+source ./chmod_all.sh
 
 sudo systemctl enable gdm
 sudo systemctl start gdm
