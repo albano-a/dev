845784e3b8294ad73c53ccbf7dbe0e0bef37f45a
André Albano <andre_barros@id.uff.br>
2025-08-25 20:55:04 -0300

    Nvim updated

--- copy_settings.sh
+++ copy_settings.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # First, copy the nvim config files
-cp -r ~/.config/nvim/* ~/personal/dev/nvim
-cp -r ~/.config/ghostty/* .~/personal/dev/ghostty
+cp -r ~/.config/nvim/* ~/personal/dev/nvim/
+cp -r ~/.config/ghostty/* ~/personal/dev/ghostty/
