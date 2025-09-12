8a70f56642ab316126f251000d92261aad850cf4
André Albano <andre_barros@id.uff.br>
2025-09-12 12:43:03 -0300

    Last update before I returned to Windows 11

--- copy_settings.sh
+++ copy_settings.sh
@@ -3,3 +3,6 @@
 # First, copy the nvim config files
 cp -r ~/.config/nvim/* ~/personal/dev/nvim/
 cp -r ~/.config/ghostty/* ~/personal/dev/ghostty/
+cp ~/.zshrc ~/personal/dev/.zshrc
+cp ~/.bashrc ~/personal/dev/.bashrc
+
